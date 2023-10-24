297df8f031b60fb26843ef7212d5328616e7d644
Joe Miller <jmiller84@hotmail.co.uk>
2023-10-24 10:40:21 +0100

    adds space model class and tests

--- lib/spaces.py
+++ lib/spaces.py
@@ -1,2 +1,13 @@
 class Space:
-    pass
+    
+    def __init__(self, id, name, description, price):
+        self.id = id 
+        self.name = name
+        self.description = description
+        self.price = price
+
+    def __repr__(self):
+        return f"Space({self.id}, {self.name}, {self.description}, {self.price})"
+    
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
