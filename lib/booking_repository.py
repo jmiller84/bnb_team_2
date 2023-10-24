4bd8c0aa6c6e9c20757059d3e6151c17b91eaf06
Joe Miller <jmiller84@hotmail.co.uk>
2023-10-24 15:34:28 +0100

    updates the booking repository class and associated tests

--- lib/booking_repository.py
+++ lib/booking_repository.py
@@ -9,3 +9,25 @@ class BookingRepository:
         rows = self._connection.execute("SELECT * FROM bookings")
         bookings = [Booking(row['id'], row['user_id'], row['space_id'], row['date']) for row in rows]
         return bookings
+    
+    def create(self, booking):
+        self._connection.execute("INSERT INTO bookings (user_id, space_id, date) VALUES (%s, %s, %s)", [booking.user_id, booking.space_id, booking.date])
+
+    
+    def find_by_user_id(self, user_id):
+        rows = self._connection.execute("SELECT * FROM BOOKINGS WHERE user_id = %s", [user_id])
+        bookings = [Booking(row['id'], row['user_id'], row['space_id'], row['date']) for row in rows]
+        return bookings
+    
+    def find_by_space_id(self, space_id):
+        rows = self._connection.execute("SELECT * FROM BOOKINGS WHERE space_id = %s", [space_id])
+        bookings = [Booking(row['id'], row['user_id'], row['space_id'], row['date']) for row in rows]
+        return bookings
+
+    def show_unavailable_dates_for_space(self, space_id):
+        bookings = self.find_by_space_id(space_id)
+        dates = [booking.date for booking in bookings]
+        return dates
+    
+    def delete(self, booking_id):
+        self._connection.execute("DELETE FROM bookings WHERE id = %s", [booking_id])
