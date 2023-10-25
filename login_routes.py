40556678485dac54ffab38508c5e449f2d9e238b
Perran Thomas <perran.thomas@icloud.com>
2023-10-25 14:47:37 +0100

    Added placeholder GET & POST route functions to login_routes.py and signup_routes.py; deleted commented-out prior route functions in app.py

--- login_routes.py
+++ login_routes.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
-from lib.booking_repository import BookingRepository
 
 def apply_login_routes(app):
-    pass
+    # Show login form page
+    @app.route('/login', methods=["GET"]) 
+    def get_login_form_page():
+        connection = get_flask_database_connection(app)
+
+    # Process submitted login form
+    @app.route('/login', methods=["POST"]) 
+    def post_login_form_data():
+        connection = get_flask_database_connection(app)
--- signup_routes.py
+++ signup_routes.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
-from lib.booking_repository import BookingRepository
 
 def apply_signup_routes(app):
-    pass
+    # Show signup form page
+    @app.route('/signup', methods=["GET"]) 
+    def get_signup_form_page():
+        connection = get_flask_database_connection(app)
+
+    # Process submitted signup form
+    @app.route('/signup', methods=["POST"])
+    def post_signup_form_data():
+        connection = get_flask_database_connection(app)
