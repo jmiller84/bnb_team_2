5b1958b1fac743c3cf6dc4f3d77c63e076ba33a1
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-10-26 12:41:53 +0100

    makes book now buttons only visible when person logged in

--- routes/homepage_routes.py
+++ routes/homepage_routes.py
@@ -5,14 +5,19 @@ from lib.booking_repository import BookingRepository
 from lib.space_repository import SpaceRepository
 from lib.user_repository import UserRepository
 from datetime import datetime
+from flask import Flask, render_template, redirect, request, session
 
 def apply_home_page_routes(app):
     @app.route('/', methods=["GET"]) 
     def home_page():
         connection = get_flask_database_connection(app)
         repository = SpaceRepository(connection)
-        spaces = repository.all()
-        return render_template('home.html', spaces=spaces)
+        if 'user_id' in session:
+            spaces = repository.all()
+            return render_template('home.html', spaces=spaces, session_active=True)
+        else:
+            spaces = repository.all()
+            return render_template('home.html', spaces=spaces, session_active=False)
     
 def apply_home_page_routes_POST(app):
     @app.route('/', methods=["POST"]) 
