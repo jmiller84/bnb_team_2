f7d1bc49492de82e6e0cd07bc78f2903b3e58212
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-10-26 14:59:06 +0100

    updates database and space class

--- lib/space.py
+++ lib/space.py
@@ -1,13 +1,14 @@
 class Space:
     
-    def __init__(self, id, name, description, price):
+    def __init__(self, id, name, description, price, user_id):
         self.id = id 
         self.name = name
         self.description = description
         self.price = price
+        self.user_id = user_id
 
     def __repr__(self):
-        return f"Space({self.id}, {self.name}, {self.description}, {self.price})"
+        return f"Space({self.id}, {self.name}, {self.description}, {self.price}, {self.user_id})"
     
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
--- seeds/MBnB.sql
+++ seeds/MBnB.sql
@@ -5,19 +5,23 @@ DROP SEQUENCE IF EXISTS users_id_seq;
 DROP TABLE IF EXISTS bookings CASCADE;
 DROP SEQUENCE IF EXISTS bookings_id_seq;
 
-CREATE SEQUENCE IF NOT EXISTS spaces_id_seq;
-CREATE TABLE spaces (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    price DECIMAL(6,2)
-);
 
 CREATE SEQUENCE IF NOT EXISTS users_id_seq;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255),
     password VARCHAR(255)
+);
+
+CREATE SEQUENCE IF NOT EXISTS spaces_id_seq;
+CREATE TABLE spaces (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    price DECIMAL(6,2),
+    user_id int,
+    constraint fk_user foreign key (user_id)
+    references users(id) ON DELETE CASCADE
 );
 
 CREATE SEQUENCE IF NOT EXISTS bookings_id_seq;
@@ -32,15 +36,16 @@ CREATE TABLE bookings (
     references spaces(id) ON DELETE CASCADE
 );
 
-INSERT INTO spaces (name, description, price) VALUES ('Cottage', 'A nice cottage', 27.50);
-INSERT INTO spaces (name, description, price) VALUES ('Villa', 'A mediterranean villa with a sea view', 70.00);
-INSERT INTO spaces (name, description, price) VALUES ('Alpine lodge', 'A cosy ski lodge with wood-burning fire', 95.00);
-
 INSERT INTO users (username, password) VALUES ('topdev', 'makers456!');
 INSERT INTO users (username, password) VALUES ('pikachu82', 'thunderbolt*');
 INSERT INTO users (username, password) VALUES ('boardgameking', 'catan1234!');
 INSERT INTO users (username, password) VALUES ('yogaguru', 'downwarddog!');
 
+INSERT INTO spaces (name, description, price, user_id) VALUES ('Cottage', 'A nice cottage', 27.50, 4);
+INSERT INTO spaces (name, description, price, user_id) VALUES ('Villa', 'A mediterranean villa with a sea view', 70.00, 2);
+INSERT INTO spaces (name, description, price, user_id) VALUES ('Alpine lodge', 'A cosy ski lodge with wood-burning fire', 95.00, 1);
+INSERT INTO spaces (name, description, price, user_id) VALUES ('Studio Apartment', 'A cool apartment in the centre of the bustling city', 102.00, 3);
+INSERT INTO spaces (name, description, price, user_id) VALUES ('Tent', 'A tent in a field of cows', 10.00, 2);
 
 INSERT INTO bookings (user_id, space_id, date) VALUES (1, 1, '2023-10-30');
 INSERT INTO bookings (user_id, space_id, date) VALUES (2, 1, '2023-09-25');
