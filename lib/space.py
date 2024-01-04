eb616b3c98795b678d2910084153464eae9a7616
Catherine Russell <catherinerussell123@hotmail.co.uk>
2024-01-04 12:50:45 +0000

    updates space and space repository to reflect new database change

--- lib/space.py
+++ lib/space.py
@@ -1,14 +1,16 @@
 class Space:
     
-    def __init__(self, id, name, description, price, owner_id):
+    def __init__(self, id, name, area, country, description, price, host_id):
         self.id = id 
         self.name = name
+        self.area = area
+        self.country = country
         self.description = description
         self.price = price
-        self.owner_id = owner_id
+        self.host_id = host_id
 
     def __repr__(self):
-        return f"Space({self.id}, {self.name}, {self.description}, {self.price}, {self.owner_id})"
+        return f"Space({self.id}, {self.name}, {self.area}, {self.country}, {self.description}, {self.price}, {self.host_id})"
     
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
--- lib/space_repository.py
+++ lib/space_repository.py
@@ -15,22 +15,22 @@ class SpaceRepository:
         rows = self._connection.execute("SELECT * FROM spaces")
         spaces = []
         for row in rows:
-            item = Space(row['id'], row['name'], row['description'], row['price'], row['owner_id'])
+            item = Space(row['id'], row['name'], row['area'], row['country'], row['description'], row['price'], row['host_id'])
             spaces.append(item)
         return spaces
     
     def create(self, space):
-        space_id = self._connection.execute("INSERT INTO spaces (name, description, price, owner_id) VALUES (%s, %s, %s, %s) RETURNING id", [space.name, space.description, space.price, space.owner_id])
+        space_id = self._connection.execute("INSERT INTO spaces (name, area, country, description, price, host_id) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id", [space.name, space.area, space.country, space.description, space.price, space.host_id])
         return space_id[0]['id']
 
     def find_by_space_name(self, space_name):
         row = self._connection.execute("SELECT * from spaces WHERE name = %s", [space_name])
-        space = Space(row[0]['id'], row[0]['name'], row[0]['description'], row[0]['price'],row['owner_id'])
+        space = Space(row[0]['id'], row[0]['name'], row[0]['area'], row[0]['country'], row[0]['description'], row[0]['price'], row[0]['host_id'])
         return space
     
     def find_by_space_id(self, space_id):
         row = self._connection.execute("SELECT * from spaces WHERE id = %s", [space_id])
-        space = Space(row[0]['id'], row[0]['name'], row[0]['description'], row[0]['price'], row[0]['owner_id'])
+        space = Space(row[0]['id'], row[0]['name'], row[0]['area'], row[0]['country'], row[0]['description'], row[0]['price'], row[0]['host_id'])
         return space
     
     
