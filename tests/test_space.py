32cce840f667a1c914df73d40e2b849613c8de8c
Catherine Russell <catherinerussell123@hotmail.co.uk>
2024-01-04 12:51:13 +0000

    updates and fixes failing tests

--- tests/test_space.py
+++ tests/test_space.py
@@ -4,26 +4,29 @@ from lib.space import Space
 Check Space constructs
 """
 def test_new_space_constructs_correctly():
-    space = Space(1, "Cottage", "A relaxing cottage", 27.50, 4)
+    space = Space(1, "Cottage", "Kent", "UK", "A relaxing cottage", 27, 4)
     assert space.id == 1
     assert space.name == "Cottage"
+    assert space.area == "Kent"
+    assert space.country == "UK"
     assert space.description == "A relaxing cottage"
-    assert space.price == 27.50
-    assert space.owner_id == 4
+    assert space.price == 27
+    assert space.host_id == 4
 
 """
 Check string representation
 """
 def test_new_space_displays_nicely():
-    space = Space(1, "Cottage", "A relaxing cottage", 27.50, 4)
-    assert str(space) == "Space(1, Cottage, A relaxing cottage, 27.5, 4)"
+    space = Space(1, "Cottage", "Kent", "UK", "A relaxing cottage", 27, 4)
+    assert str(space) == "Space(1, Cottage, Kent, UK, A relaxing cottage, 27, 4)"
 
 """
 Check 2 spaces with same attributes are equal
 """
 def test_spaces_are_equal():
-    space1 = Space(1, "Cottage", "A relaxing cottage", 27.50, 1)
-    space2 = Space(1, "Cottage", "A relaxing cottage", 27.50, 1)
+    space1 = Space(1, "Cottage", "Kent", "UK", "A relaxing cottage", 27, 4)
+    space2 = Space(1, "Cottage", "Kent", "UK", "A relaxing cottage", 27, 4)
+
     assert space1 == space2
 
 """
--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -3,40 +3,46 @@ from lib.space import Space
 
 """
 Tests:
-    - when we create a new space, it is displayed in the list of all spaces
     - when we read all spaces, we see a list of all spaces
+    - when we create a new space, it is displayed in the list of all spaces
     - When we call find_space_by_title, we see the space displayed
+    TODO:
     - when we update a space, we see the change in the list of all spaces X
     - when we delete a space X
 """
-# - when we create a new space, it is displayed in the list of all spaces
 
+# when we read all spaces, we see a list of all spaces in database
 
 def test_list_all_spaces(db_connection):
     db_connection.seed("seeds/MBnB.sql")
     repository = SpaceRepository(db_connection)
     assert repository.all() == [
-        Space(1, 'Cottage', 'A nice cottage', 27.50, 4),
-        Space(2, 'Villa', 'A mediterranean villa with a sea view', 70.00, 2),
-        Space(3, 'Alpine lodge', 'A cosy ski lodge with wood-burning fire', 95.00, 3),
-        Space(4, 'Studio Apartment', 'A cool apartment in the centre of the bustling city', 102.00, 3),
-        Space(5, 'Tent', 'A tent in a field of cows', 10.00, 2)
+        Space(1, 'Cottage', 'Wimborne', 'UK', 'A nice cottage', 27, 4),
+        Space(2, 'Villa', 'Nice', 'France', 'A mediterranean villa with a sea view', 70, 2),
+        Space(3, 'Alpine lodge', 'Brig-Glis', 'Switzerland', 'A cosy ski lodge with wood-burning fire', 95, 1),
+        Space(4, 'Studio Apartment', 'New York', 'USA', 'A cool apartment in the centre of the bustling city', 102, 3),
+        Space(5, 'Tent', 'Tenby', 'UK', 'A tent in a field of cows', 10, 2)
     ]
 
 
+# - when we create a new space, it is displayed in the list of all spaces
 def test_create_a_new_space(db_connection):
     db_connection.seed("seeds/MBnB.sql")
     repository = SpaceRepository(db_connection)
-    repository.create(Space(None, 'Studio Flat', 'A cool city-centre location', 50.00, 3))
+    repository.create(Space(None, 'Cabin', 'Tampaksiring', 'Indonesia', 'Located just a 20-minute scooter ride from the vibrant centre of Ubud, the Dome is a newly built one bedroom bamboo cabin situated within the Eco Six Resort.', 230, 3))
     assert repository.all() == [
-        Space(1, 'Cottage', 'A nice cottage', 27.50, 4),
-        Space(2, 'Villa', 'A mediterranean villa with a sea view', 70.00, 2),
-        Space(3, 'Alpine lodge', 'A cosy ski lodge with wood-burning fire', 95.00, 1),
-        Space(4, 'Studio Flat', 'A cool city-centre location', 50.00, 3)
+        Space(1, 'Cottage', 'Wimborne', 'UK', 'A nice cottage', 27, 4),
+        Space(2, 'Villa', 'Nice', 'France', 'A mediterranean villa with a sea view', 70, 2),
+        Space(3, 'Alpine lodge', 'Brig-Glis', 'Switzerland', 'A cosy ski lodge with wood-burning fire', 95, 1),
+        Space(4, 'Studio Apartment', 'New York', 'USA', 'A cool apartment in the centre of the bustling city', 102, 3),
+        Space(5, 'Tent', 'Tenby', 'UK', 'A tent in a field of cows', 10, 2),
+        Space(6, 'Cabin', 'Tampaksiring', 'Indonesia', 'Located just a 20-minute scooter ride from the vibrant centre of Ubud, the Dome is a newly built one bedroom bamboo cabin situated within the Eco Six Resort.', 230, 3)
     ]
+
+# When we call find_space_by_title, we see the space displayed
 
 def test_find_space_by_name_returns_single_space(db_connection):
     db_connection.seed("seeds/MBnB.sql")
     repository = SpaceRepository(db_connection)
-    space = repository.find_by_name("Alpine lodge")
-    assert space == Space(3, 'Alpine lodge', 'A cosy ski lodge with wood-burning fire', 95.00)
+    space = repository.find_by_space_name("Alpine lodge")
+    assert space == Space(3, 'Alpine lodge', 'Brig-Glis', 'Switzerland', 'A cosy ski lodge with wood-burning fire', 95, 1)
