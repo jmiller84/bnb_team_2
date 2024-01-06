9212601ba7eb0bdf1a41c6eaeca3636c43548dae
Catherine Russell <catherinerussell123@hotmail.co.uk>
2024-01-06 20:20:50 +0000

    updates database with information and pics

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -10,8 +10,8 @@ from psycopg.rows import dict_row
 # That's why we have provided it!
 class DatabaseConnection:
     # VVV CHANGE BOTH OF THESE VVV
-    DEV_DATABASE_NAME = "bnb_team_2"
-    TEST_DATABASE_NAME = "bnb_team_2_test"
+    DEV_DATABASE_NAME = "mbnb"
+    TEST_DATABASE_NAME = "mbnb"
 
     def __init__(self, test_mode=False):
         self.test_mode = test_mode
